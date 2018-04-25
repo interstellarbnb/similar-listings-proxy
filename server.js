32e2e03f43b5bff250e38ae2395e98b491488a75
Evelyn <evelynchan92@gmail.com>
2018-04-24 17:21:32 -0700

    rendered components

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
@@ -6,3 +7,4 @@ const port = 3000;
 app.listen(port, () => console.log(`App live on http://localhost:${port}`));
 
 app.use('/:id', express.static('public'));
+app.use(cors());
